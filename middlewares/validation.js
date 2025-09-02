d0f72a7a381b97827d47e0e60d970fd04da33346
KAY <karen.aldana012@gmail.com>
2025-09-02 08:00:11 -0400

    errror updates

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -29,7 +29,7 @@ module.exports.validateCardBody = celebrate({
 
 module.exports.validateUserCreation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).messages({
+    name: Joi.string().min(2).max(30).required().messages({
       "string.min": 'The minimum length of the "name" field is 2',
       "string.max": 'The maximum length of the "name" field is 30',
       "string.empty": 'The "name" field must be filled in',
